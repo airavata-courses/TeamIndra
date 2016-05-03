764314d7b352651720667ab0c389c1415a06a217
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-02 21:11:27 -0400

    Added Appspec and install script

--- Gateway Web Service/install.sh
+++ Gateway Web Service/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
-mvn -f /home/ec2-user/builds/Gateway\ Web\ Service/pom.xml clean;
-mvn -f /home/ec2-user/builds/Gateway\ Web\ Service/pom.xml install;
-chmod a+w /home/ec2-user/builds/Gateway\ Web\ Service/target
-sudo -u ec2-user java -jar /home/ec2-user/builds/Gateway\ Web\ Service/target/indra-1.0.0-BUILD-SNAPSHOT.war &
+mvn -f /home/ec2-user/builds/Gateway Web Service/pom.xml clean;
+mvn -f /home/ec2-user/builds/Gateway Web Service/pom.xml install;
+chmod a+w /home/ec2-user/builds/Gateway Web Service/target
+sudo -u ec2-user java -jar /home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war &
