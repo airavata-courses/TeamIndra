8010fd16b64fa7c1c9aded72f5d18777627f7612
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-02 22:30:52 -0400

    Changed Admin

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
 chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
-sudo -u ec2-user java -jar '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' &
+cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+sh '/home/ec2-user/apache-tomcat-8.0.33/bin/startup.sh' 
