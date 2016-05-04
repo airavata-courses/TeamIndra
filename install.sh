3f7feea7a7a4a0835fdffe01d690a0e932f62a62
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-04 16:16:57 -0400

    Added Micro-services

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
+
+mvn -f '/home/ec2-user/builds/Auth Service/pom.xml' clean;
+mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
+chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
+cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+
+mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
+mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
+chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
+cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+
+
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
 chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
