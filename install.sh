b1cab6c055cc2b5080ab8d019c0325caaba5ebc4
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-04 23:01:54 -0400

    Test

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ chmod a+w '/home/ec2-user/builds/Data Service/target'
 cp '/home/ec2-user/builds/Data Service/target/indra-dao-1.0.0.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
 
 sh '/home/ec2-user/apache-tomcat-8.0.33/bin/startup.sh'
-
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
 chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
