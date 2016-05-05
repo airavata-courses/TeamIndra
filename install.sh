b29cc7c2a3a59d730b92701b36351a443ab659c6
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-04 22:46:50 -0400

    Modified installation script

--- install.sh
+++ install.sh
@@ -3,18 +3,19 @@
 ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
 
 mvn -f '/home/ec2-user/builds/Auth Service/pom.xml' clean;
-mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
-chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
-cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+mvn -f '/home/ec2-user/builds/Auth Service/pom.xml' install;
+chmod a+w '/home/ec2-user/builds/Auth Service/target'
+cp '/home/ec2-user/builds/Auth Service/target/indra-auth-1.0.0.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+
+mvn -f '/home/ec2-user/builds/Data Service/pom.xml' clean;
+mvn -f '/home/ec2-user/builds/Data Service/pom.xml' install;
+chmod a+w '/home/ec2-user/builds/Data Service/target'
+cp '/home/ec2-user/builds/Data Service/target/indra-dao-1.0.0.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
+
+sh '/home/ec2-user/apache-tomcat-8.0.33/bin/startup.sh'
 
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
 mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
 chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
 cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
-
-
-mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
-mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
-chmod a+w '/home/ec2-user/builds/Gateway Web Service/target'
-cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
-sh '/home/ec2-user/apache-tomcat-8.0.33/bin/startup.sh' 
+ 
