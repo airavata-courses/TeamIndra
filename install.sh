9a3e5e184d772d04ba71c38a76c2de53071e0cdc
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2016-05-06 09:43:56 -0400

    CHanges

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
-
-
+sudo ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
 sudo mvn -f '/home/ec2-user/builds/Auth Service/pom.xml' clean;
 sudo mvn -f '/home/ec2-user/builds/Auth Service/pom.xml' install;
 sudo cp '/home/ec2-user/builds/Auth Service/target/indra-auth-1.0.0.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
@@ -13,5 +11,4 @@ sudo cp '/home/ec2-user/builds/Data Service/target/indra-dao-1.0.0.war' '/home/e
 sudo sh '/home/ec2-user/apache-tomcat-8.0.33/bin/startup.sh'
 sudo mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' clean;
 sudo mvn -f '/home/ec2-user/builds/Gateway Web Service/pom.xml' install;
-cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
- 
+sudo cp '/home/ec2-user/builds/Gateway Web Service/target/indra-1.0.0-BUILD-SNAPSHOT.war' '/home/ec2-user/apache-tomcat-8.0.33/webapps'
